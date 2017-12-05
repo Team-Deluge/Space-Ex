0eee01c3f88f452d0ad4ee5390459c90eb367f09
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2017-12-04 23:26:30 -0800

    Redux file structure is mostly in place

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,16 @@
 import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Signup from './Signup';
-import OwnerDash from './OwnerDash';
-import RenterDash from './RenterDash';
+import OwnerContainer from '../containers/OwnerContainer';
+import RenterContainer from '../containers/RenterContainer';
 import Login from './Login';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Login} />
     <Route path="/signup" component={Signup} />
-    <Route path="/ownerdash" component={OwnerDash} />
-    <Route path="/renterdash" component={RenterDash} />
+    <Route path="/owner" component={OwnerContainer} />
+    <Route path="/renter" component={RenterContainer} />
   </Switch>
 );
 
---
+++ client/constants/actionTypes.js
@@ -0,0 +1,4 @@
+// add action type constants i.e.:
+const ACTION_DESCRIPTION = 'ACTION_DESCRIPTION';
+
+// export all action types, which will be used by actions.js
